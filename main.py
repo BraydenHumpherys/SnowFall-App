64a8f7d160983b7da9675e0e707e01a534a2af42
Brayden Humpherys <21brayden.rich@gmail.com>
2025-04-16 21:52:15 -0600

    Finish App

--- main.py
+++ main.py
@@ -10,29 +10,45 @@ df = pd.read_csv(url)
 df['time'] = pd.to_datetime(df['time'])
 df['month'] = df['time'].dt.month
 df['year'] = df['time'].dt.year
+df['month_year'] = df['time'].dt.to_period('M').astype(str)
+
 
 
 st.title('Cracking the Snow Code')
 
 tab1, tab2, tab3 = st.tabs(['Snowfall By Month', 'Time Series', 'Heat Map'])
 
+season_mapping = {
+        "23-24": ["2023-12", "2024-01", "2024-02", "2024-03"],
+        "24-25": ["2024-12", "2025-01", "2025-02"]
+    }
 
 # User Selects year, and a graph of the the snowfall by month is shown.
 with tab1: 
-    selected_year = st.selectbox('Select Year', options=df['year'].unique())
-    monthly_snowfall = df[df['year'] == selected_year].groupby('month')['snowfall_sum'].sum()
+    selected_season = st.selectbox('Select Snow Season', options=season_mapping.keys())
+
+    filtered_months = season_mapping[selected_season]
+    seasonal_data = df[df['month_year'].isin(filtered_months)]
+
+    monthly_snowfall = seasonal_data.groupby('month_year')['snowfall_sum'].sum()
 
     fig, ax = plt.subplots()
     sns.barplot(x=monthly_snowfall.index, y=monthly_snowfall.values, ax=ax)
-    ax.set_title(f'Snowfall By Month for {selected_year}')
-    ax.set_xlabel('Month')
+    ax.set_title(f'Snowfall by Month-Year for Season {selected_season}')
+    ax.set_xlabel('Month-Year')
     ax.set_ylabel('Total Snowfall')
+    plt.xticks(rotation=45)
     st.pyplot(fig)
 
 
  # User selects variables shown on a time series
  #expander is present to show the variable given
 with tab2:
+
+    selected_season = st.selectbox('Select Snow Season for Time Series', options=season_mapping.keys(), key='season_ts')
+    filtered_months = season_mapping[selected_season]
+    seasonal_df = df[df['month_year'].isin(filtered_months)]
+
     variables = st.multiselect('Select Variables for Time Series', 
                                options=['temperature_2m_mean', 'precipitation_hours', 
                                         'wind_speed_10m_max', 'shortwave_radiation_sum'], 
@@ -48,10 +64,10 @@ with tab2:
     for var in variables:
         expander.write(f"{var}: {variables_explanation[var]}")
 
-    time_series_data = df[['time'] + variables].set_index('time')
+    time_series_data = seasonal_df[['time'] + variables].set_index('time')
     fig, ax = plt.subplots()
     sns.lineplot(data=time_series_data, ax=ax)
-    ax.set_title('Time Series of Selected Variables')
+    ax.set_title(f'Time Series of Selected Variables ({selected_season} Season)')
     ax.set_xlabel('Time')
     ax.set_ylabel('Values')
     st.pyplot(fig)
